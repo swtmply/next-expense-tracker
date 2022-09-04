136823f6d25ecfb420057f8317df92f73f6bb220
swtmply <delosreyesjohnallen@gmail.com>
2022-09-04 19:17:21 +0800

    🚀 meta: added PWA

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from "next/document"
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -8,11 +8,14 @@ export default function Document() {
           rel="stylesheet"
           href="https://kit-pro.fontawesome.com/releases/v6.1.1/css/pro.css"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/images/icon-192x192.png"></link>
+        <meta name="theme-color" content="#fff" />
       </Head>
       <body className="bg-light-background text-light-primary dark:bg-dark-background dark:text-dark-primary transition-colors min-h-screen selection:bg-purple-300 selection:text-light-primary">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
